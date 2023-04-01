dac9cc5ca4d5a203a9491d5f1b687fcff04d3d46
Ravi <dawaderavi4704@gmail.com>
2023-04-01 16:57:04 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -18,4 +18,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("division error")
+        raise CustomException(e,sys)
 
